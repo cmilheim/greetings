f589a89d11edaa6e68d5127de003610460ec9bfe
Chris Milheim <cmilheim@homes.com>
2022-05-24 14:36:47 -0500

    Fix type

--- greetings.go
+++ greetings.go
@@ -3,7 +3,7 @@ package greetings
 import "fmt"
 
 // Hello returns a greeting for the named person
-func Hell(name string) string {
+func Hello(name string) string {
 	// Return a greeting that embeds the name in the message.
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
 	return message
