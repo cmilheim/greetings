3215a11ac7e19e68db49068c6abd6dfe48e2a6ce
Chris Milheim <cmilheim@homes.com>
2022-05-24 15:17:33 -0500

    I'M STILL LEARNING!

--- greetings.go
+++ greetings.go
@@ -17,29 +17,28 @@ func Hello(name string) (string, error) {
 	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 	/*
-	//If a name was received, return a value that embeds the name
-	// in a greeting message.
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message, nil
+		//If a name was received, return a value that embeds the name
+		// in a greeting message.
+		message := fmt.Sprintf("Hi, %v. Welcome!", name)
+		return message, nil
 	*/
+}
 
-	// init sets initial values for variables use din the fucntion.
-	func init() { 
-		rand.Seed(time.Now().UnixNano())
+// init sets initial values for variables use din the fucntion.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+// randomFormat returns one of a set of greeting messages. The returned
+// message is selected at random.
+func randomFormat() string {
+	// A slice of message formats.
+	formats := []string{
+		"Hi, %v. Welcome!",
+		"Great to see you, %v!",
+		"Hail, $v! Well met!",
 	}
-
-	// randomFormat returns one of a set of greeting messages. The returned
-	// message is selected at random.
-	func randomFormat() string {
-		// A slice of message formats.
-		format := []string{
-			"Hi, %v. Welcome!",
-			"Great to see you, %v!",
-			"Hail, $v! Well met!",
-		}
-
-		// Return a randomly selected message format by specifying
-		// a random index for the slice of formats.
-		return formats[rand.Intn(len(formats))]
-	}
+	// Return a randomly selected message format by specifying
+	// a random index for the slice of formats.
+	return formats[rand.Intn(len(formats))]
 }
